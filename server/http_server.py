588cece316674bf1c6edf355174c63e86b636c0b
Denys Lazarenko <denis.lazarenko@gmail.com>
2015-08-23 20:11:47 +0200

    HTTP server implemented in Python. GET request treated

---
+++ server/http_server.py
@@ -0,0 +1,45 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import time
+import numpy as np
+
+
+def get_tweets(search_tag):
+	"""
+	STUB (no connection to Twitter for a moment)
+	Generating a random sequence of N numbers, where N is the length of a query tag.
+	"""
+	N = len(search_tag)
+	scores = np.random.randint(30, size=N)
+	return ",".join([str(score) for score in scores])
+
+
+class MyHandler(BaseHTTPRequestHandler):
+	def do_GET(self):
+		self.send_response(200)
+		self.send_header("Content-type", "application/json")
+		self.end_headers()
+
+		search_tag = self.path[1:]
+		tweets_string = get_tweets(search_tag)
+		
+		self.wfile.write(bytes("{\
+			\"searchTag\": \"" + search_tag + "\",\
+			\"result\": \"" + tweets_string + "\"}",\
+			 "utf-8"))
+
+
+
+HOST = "localhost"
+PORT = 8822
+
+
+my_server = HTTPServer((HOST, PORT), MyHandler)
+print(time.asctime(), "Server Starts - %s:%s" % (HOST, PORT))
+
+try:
+	my_server.serve_forever()
+except KeyboardInterrupt:
+	pass
+
+my_server.server_close()
+print(time.asctime(), "Server Stops - %s:%s" % (HOST, PORT))
